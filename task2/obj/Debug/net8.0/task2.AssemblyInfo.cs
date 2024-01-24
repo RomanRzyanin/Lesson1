b8586a2741dd44ee5adaf527543996670af5f895
Roman Rzyanin <roman@rzyanin.ru>
2024-01-24 22:27:29 +0300

    add product and quotient

--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cbdb7f6353b34bf8cfb95c001707cba9f2dbc3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33cfbe0e276ee73fd6810ef8fa4b2e1c71c7619")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
