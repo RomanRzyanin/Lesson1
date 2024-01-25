0f625a8a7adf1db6ff0d0b947efa52eeb0566f2e
Roman Rzyanin <roman@rzyanin.ru>
2024-01-25 22:36:10 +0300

    Solving homework

---
+++ homework1/Program.cs
@@ -0,0 +1,78 @@
+﻿// Home_work
+// Task1
+
+// int firstNumber = 5;
+// int secondNumber = 6;
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine("первое число больше");
+// }
+// if (firstNumber < secondNumber)
+// {
+//     Console.WriteLine("второе число больше");
+// }
+// if (firstNumber == secondNumber)
+// {
+//     Console.WriteLine("числа равны");
+// }
+
+// Task2
+
+// int a = 7;
+// int b = 7;
+// int c = 7;
+// //int max = a;
+// if (a > b & a > c)
+// {
+//     Console.WriteLine("max = " + a);
+// }
+// if (b > a & b > c)
+// {
+//     Console.WriteLine("max = " + b);
+// }
+// if (c > b & c > a)
+// {
+//     Console.WriteLine("max = " + c);
+// }
+// if (a == b & b == c)
+// {
+//     Console.WriteLine("The numbers are equai");
+// }
+// if (a > b & a > c)
+// {
+//     FindMax = a;
+// }
+// if (b > a & b > c)
+// {
+//     FindMax = b;
+// }
+// if (c > b & c > a)
+// {
+//     FindMax = c;
+// }
+// return FindMax;
+
+//Task3
+
+// int number = 7;
+// if (number % 2 == 0)
+// {
+//    Console.WriteLine("четное"); 
+// }
+// else
+// {
+//     Console.WriteLine("нечетное");
+// }
+
+//Task4
+
+int number = 100;
+int i = 1;
+while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
